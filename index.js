8f3a4107ceca23944c7d2f60b4432bec5edd8196
harleymacedo <harley.ip@gmail.com>
2022-11-21 16:07:07 -0300

    Stream de vídeo funcionando

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 app.get('/', (req, res) => {
     res.json({mensagem: 'Funcionando corretamente'});
+});
+
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/video', (req, res) => {
+    const range = req.headers.range;
+    const videoPath = './toyotaPrius.mp4';
+    const videoSize = fs.statSync(videoPath).size;
+
+    const chunkSize = 1 * 1e+6;
+    const start = Number(range.replace(/\D/g, ''));
+    const end = Math.min(start + chunkSize, videoSize - 1);
+
+    const contentLength = end - start + 1;
+
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4",
+    }
+    res.writeHead(206, headers);
+
+    const stream = fs.createReadStream(videoPath, {start, end});
+    stream.pipe(res);
 });
 
 app.listen(3000, () => {
