bf82f53243158a92ecb423aef810e576528d9ec9
harleymacedo <harley.ip@gmail.com>
2022-11-22 10:42:14 -0300

    Audio por demanda funcionando normalmente

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ app.get('/', (req, res) => {
     res.json({mensagem: 'Funcionando corretamente'});
 });
 
-app.get('/home', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+app.get('/homeVideo', (req, res) => {
+    res.sendFile(__dirname + '/videos.html');
+});
+
+app.get('/homeMusica', (req, res) => {
+    res.sendFile(__dirname + '/musicas.html');
 });
 
 app.get('/video', (req, res) => {
@@ -30,6 +34,29 @@ app.get('/video', (req, res) => {
     res.writeHead(206, headers);
 
     const stream = fs.createReadStream(videoPath, {start, end});
+    stream.pipe(res);
+});
+
+app.get('/musica', (req, res) => {
+    const range = req.headers.range;
+    const musicaPath = './musica1.mp3';
+    const musicaSize = fs.statSync(musicaPath).size;
+
+    const chunkSize = 1 * 1e+4;
+    const start = Number(range.replace(/\D/g, ''));
+    const end = Math.min(start + chunkSize, musicaSize - 1);
+
+    const contentLength = end - start + 1;
+
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${musicaSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "audio/mp3",
+    }
+    res.writeHead(206, headers);
+
+    const stream = fs.createReadStream(musicaPath, {start, end});
     stream.pipe(res);
 });
 
